e5849f1b48a09d042dfc1c2ea54c635648e7b7f0
Toshiya Saito <saitou@ialab.cs.tsukuba.ac.jp>
2023-09-08 17:50:24 +0900

    add: CIで`cargo clippy`を行うよう変更

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ pub extern "C" fn increment(n: u32) -> u32 {
     n + 1
 }
 
+#[allow(dead_code)]
+fn sum(array: &Vec<i32>) -> i32 {
+    array.iter().sum()
+}
+
 #[cfg(test)]
 #[test]
 fn increment_test() {
