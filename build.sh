f71866276834f81379af8c7bf936fd48bfbea78a
Toshiya Saito <saitou@ialab.cs.tsukuba.ac.jp>
2023-09-12 15:43:01 +0900

    add: tree-sitter-* への依存を追加

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 source ../emsdk/emsdk_env.sh
 
+export EMCC_CFLAGS="-O3"
+
+cargo build --package tree-sitter-json --target wasm32-unknown-emscripten --release
+
 export EMCC_CFLAGS="-O3
-                    -o ./output/increment.js
-                    -s EXPORTED_FUNCTIONS=['_increment']
+                    -o ./public/increment.js
+                    -s EXPORTED_FUNCTIONS=['_increment','_get_json_cst','_free_string']
                     -s EXPORTED_RUNTIME_METHODS=ccall"
 
 cargo build --target wasm32-unknown-emscripten --release --lib
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,32 @@
+use std::ffi::{c_char, CStr, CString};
+
 #[no_mangle]
 pub extern "C" fn increment(n: u32) -> u32 {
     n + 1
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn get_json_cst(json_str: *mut c_char) -> *mut c_char {
+    let json_str = CStr::from_ptr(json_str).to_str().unwrap().to_owned();
+
+    CString::new(get_json_cst_core(&json_str))
+        .unwrap()
+        .into_raw()
+}
+
+fn get_json_cst_core(json_str: &str) -> String {
+    let mut parser = tree_sitter::Parser::new();
+    parser.set_language(tree_sitter_json::language()).unwrap();
+    let tree = parser.parse(json_str, None).unwrap();
+
+    format!("{:?}", tree)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_string(ptr: *mut c_char) {
+    if !ptr.is_null() {
+        let _ = CString::from_raw(ptr);
+    };
 }
 
 #[allow(dead_code)]
@@ -12,4 +38,14 @@ fn sum(array: &[i32]) -> i32 {
 #[test]
 fn increment_test() {
     assert_eq!(increment(5), 5 + 1)
+}
+
+#[cfg(test)]
+#[test]
+fn get_json_cst_test() {
+    let src = r#"{hoge: "fuga"}"#;
+    assert_eq!(
+        get_json_cst_core(src),
+        "{Tree {Node document (0, 0) - (0, 14)}}".to_owned()
+    )
 }
