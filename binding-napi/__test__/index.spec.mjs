c7068efb20272e77e73fffc82207d7b56e6ac0d2
Toshiya Saito <saitou@ialab.cs.tsukuba.ac.jp>
2023-09-12 18:54:08 +0900

    remove: 自作のテストを削除

--- binding-napi/__test__/index.spec.mjs
+++ binding-napi/__test__/index.spec.mjs
@@ -1,14 +1,14 @@
 import test from "ava";
 
 import { sum } from "../index.js";
-import { getJsonCst } from "../index.js";
+// import { getJsonCst } from "../index.js";
 
 test("sum from native", (t) => {
   t.is(sum(1, 2), 3);
 });
 
-test("get_json_cst", (t) => {
-  const json = { hoge: "fuga" };
-  const jsonStr = JSON.stringify(json);
-  t.is(getJsonCst(jsonStr), "{Tree {Node document (0, 0) - (0, 15)}}");
-});
+// test("get_json_cst", (t) => {
+//   const json = { hoge: "fuga" };
+//   const jsonStr = JSON.stringify(json);
+//   t.is(getJsonCst(jsonStr), "{Tree {Node document (0, 0) - (0, 15)}}");
+// });
